a32e13af087ecd041182f993002730fc33dc62c0
Colombo <lepersorium@gmail.com>
2020-11-07 12:10:13 +0400

    fix saveables

--- litenn/core/module/Dropout.py
+++ litenn/core/module/Dropout.py
@@ -6,14 +6,16 @@ class Dropout(nn.Module):
     Dropout module.
 
         Parameters
-        
+
         rate       float    [0.0 .. 1.0)
-        
+
     reference
-    
+
     Dropout: A Simple Way to Prevent Neural Networks from Overfitting
     """
     def __init__(self, rate=0.5):
+        super().__init__(saveables=[], trainables=[])
+
         if rate < 0 or rate >= 1.0:
             raise ValueError(f'rate {rate} must be in range [0 .. 1.0)')
         self.rate = rate
@@ -21,20 +23,20 @@ class Dropout(nn.Module):
     def forward(self, x, **kwargs):
         if self.is_training():
             x = nn.dropout(x, self.rate)
-        return x            
-        
+        return x
+
     def __str__(self): return f"{self.__class__.__name__} : rate:{self.rate} "
     def __repr__(self): return self.__str__()
 
 def Dropout_test():
     module = Dropout(0.3)
-    
-    module.set_training(True)        
+
+    module.set_training(True)
     x = nn.Tensor( (2,4,4,4) )
     y = module(x)
     y.backward(grad_for_non_trainables=True)
-    
-    module.set_training(False)        
+
+    module.set_training(False)
     x = nn.Tensor( (2,4,4,4) )
     y = module(x)
     y.backward(grad_for_non_trainables=True)
--- litenn/core/module/FRNorm2D.py
+++ litenn/core/module/FRNorm2D.py
@@ -4,16 +4,16 @@ import litenn as nn
 class FRNorm2D(nn.Module):
     """
     Filter Response Normalization.
-    
+
     should be used with nn.TLU() activator
-    
+
     arguments
-    
+
         in_ch       input channels
-    
-    reference 
-    
-    Filter Response Normalization Layer: 
+
+    reference
+
+    Filter Response Normalization Layer:
     Eliminating Batch Dependence in the Training of Deep Neural Networks
     https://arxiv.org/pdf/1911.09737.pdf
     """
@@ -24,20 +24,22 @@ class FRNorm2D(nn.Module):
         self.bias        = nn.Tensor ( (in_ch,), init=nn.initializer.Scalar(0.0) )
         self.eps         = nn.Tensor ( (1,),  init=nn.initializer.Scalar(1e-6) )
 
+        super().__init__(saveables=['weight', 'bias', 'eps'])
+
     def forward(self, x):
         nu2 = nn.square(x).mean( (-2,-1), keepdims=True)
-        
+
         x = x * ( 1.0/nn.sqrt(nu2 + nn.abs(self.eps) ) )
 
         return x*self.weight.reshape ( (1,-1,1,1) ) \
                + self.bias.reshape   ( (1,-1,1,1) )
-        
+
 def FRNorm2D_test():
     in_ch = 3
-    module = FRNorm2D(in_ch)        
+    module = FRNorm2D(in_ch)
     x = nn.Tensor( (2,in_ch,64,64) )
     y = module(x)
     y.backward(grad_for_non_trainables=True)
-    
+
     if not x.has_grad():
         raise Exception(f'x has no grad')
